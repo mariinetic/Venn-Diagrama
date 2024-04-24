789c19168fb90eab1e8bd834c332020d15e46706
Mariana Oliveira <162724918+mariinetic@users.noreply.github.com>
2024-04-24 17:12:35 -0300

    Update resultado.js

--- resultado.js
+++ resultado.js
@@ -1,16 +1,15 @@
-  // Atualiza o diagrama quando o botão é clicado
-        function updateVennDiagram() {
-            const A = document.getElementById('a').value.replace(".", ",") || ""; // Joga em A
-            const B = document.getElementById('b').value.replace(".", ",") || ""; // Joga em B
-            const C = document.getElementById('c').value.replace(".", ",") || ""; // Joga em C
+function updateVennDiagram() {
+  const A = document.getElementById('a').value.replace(".", ",") || ""; // Joga em A
+  const B = document.getElementById('b').value.replace(".", ",") || ""; // Joga em B
+  const C = document.getElementById('c').value.replace(".", ",") || ""; // Joga em C
 
-            // No minimo 10 valores
-            const aText = a.length > 10 ? a.substring(0, 15) : a;
-            const bText = b.length > 10 ? b.substring(0, 15) : b;
-            const cText = c.length > 10 ? c.substring(0, 15) : c;
+  // No minimo 10 valores
+  const textoA = A.length > 10 ? A.substring(0, 15) : A;
+  const TextoB = B.length > 10 ? B.substring(0, 15) : B;
+  const TextoC = C.length > 10 ? C.substring(0, 15) : C;
 
-            // Joga os números pra dentro do diagrama
-            document.getElementById('textA').textContent = aText;
-            document.getElementById('textB').textContent = bText;
-            document.getElementById('textC').textContent = cText;
-        }
+  // Joga os números pra dentro do diagrama
+  document.getElementById('textA').textContent = textoA;
+  document.getElementById('textB').textContent = TextoB;
+  document.getElementById('textC').textContent = TextoC;
+}
